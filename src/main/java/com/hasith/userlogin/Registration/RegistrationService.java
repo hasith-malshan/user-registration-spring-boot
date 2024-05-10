dd4d476db2e57f86555308a4ffff7501da63dc4a
hmr53 <h.malshan.53@gmail.com>
2024-05-10 21:40:35 +0530

    register method implemented to register users.

--- src/main/java/com/hasith/userlogin/Registration/RegistrationService.java
+++ src/main/java/com/hasith/userlogin/Registration/RegistrationService.java
@@ -1,10 +1,29 @@
 package com.hasith.userlogin.Registration;
 
+import com.hasith.userlogin.appuser.AppUser;
+import com.hasith.userlogin.appuser.AppUserRole;
+import com.hasith.userlogin.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "Works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail){
+            throw new IllegalStateException("Email Not Valid");
+        }
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
     }
 }
