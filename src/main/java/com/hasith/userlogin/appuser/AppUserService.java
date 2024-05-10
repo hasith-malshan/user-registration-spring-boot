a8e51cdd28fc095250a425f568ea83d39ad7e86b
hmr53 <h.malshan.53@gmail.com>
2024-05-10 21:39:51 +0530

    register method implemented to register users.

--- src/main/java/com/hasith/userlogin/appuser/AppUserService.java
+++ src/main/java/com/hasith/userlogin/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,9 +12,24 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final static String USER_NOT_FOUND_MESSAGE = "User with email %s not found";
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return appUserRepository.findByEmail(email).orElseThrow(()-> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MESSAGE,email)));
+        return appUserRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MESSAGE, email)));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+        if (userExists) {
+            throw new IllegalStateException("Email Already exists");
+        }
+        String encodePassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodePassword);
+        appUserRepository.save(appUser);
+        // TODO: 5/10/2024 Send Confirmation token
+        return "It Works";
     }
 }
